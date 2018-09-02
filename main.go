aa0a11d0924d199485bd69df8423e60437007c50
Tetsuya Yamada <yamada.tetsuya258@gmail.com>
2018-09-02 17:32:10 +0900

    add switch cloud

--- main.go
+++ main.go
@@ -3,29 +3,40 @@ package main
 import (
     "fmt"
     "os"
+    "strings"
     "github.com/c-bata/go-prompt"
 )
 
 func executer(in string) {
+    fmt.Println("Your input: " + in)
+
     if in == "exit" {
         os.Exit(0)
-    } else {
-        fmt.Println("Your input: " + in)
+    } else if in == "help" {
+        fmt.Println("command format: cloud service [action]")
+    }
 
+    switch parse(in)[0] {
+    case "gcp":
+        fmt.Println("Google Cloud Platform")
+    case "aws":
+        fmt.Println("Amazon Web Service")
     }
 }
 
 func completer(d prompt.Document) []prompt.Suggest {
     s := []prompt.Suggest{
-		{Text: "users", Description: "Store the username and age"},
-		{Text: "articles", Description: "Store the article text posted by user"},
-		{Text: "comments", Description: "Store the text commented to articles"},
 	}
 	return prompt.FilterHasPrefix(s, d.GetWordBeforeCursor(), true)
 }
 
+func parse(in string) []string {
+    s := strings.Split(in, " ")
+    return s
+}
+
 func main() {
-    p := prompt.New (
+    p := prompt.New(
         executer,
         completer,
         prompt.OptionPrefix(">>> "),
